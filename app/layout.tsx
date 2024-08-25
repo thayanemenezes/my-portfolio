b16e93613316b14f71cb347acb095a0b18900eb9
Thayane Menezes <thayane.nascimento@solution4fleet.com.br>
2024-08-25 20:05:01 -0300

    feat: change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My portfolio",
+  title: "Thayane Menezes",
   description: "Web & mobile portfolio",
 };
 
