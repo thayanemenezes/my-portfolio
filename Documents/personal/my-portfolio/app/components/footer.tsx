a1a18ab1feb1d98e10c817b2d3f6f4c3a9ace004
Thayane Menezes <thayane.nascimento@solution4fleet.com.br>
2024-08-25 18:49:45 -0300

    feat: first commit

---
+++ Documents/personal/my-portfolio/app/components/footer.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <hr />
+      <div className="lg:flex sm:flex-col lg:flex-row w-full align-center justify-between py-10">
+        <div className="w-full">
+          <h2 className="text-gray-900">Get in touch! 👋</h2>
+          <h4 className="text-gray-900 ">thayanemenezes.dev@gmail.com</h4>
+        </div>
+        <div className="flex flex-row w-32 justify-between items-center mt-5">
+          <Link
+            href={"https://www.linkedin.com/in/thayane-menezes-31a5a9191/"}
+            className="origin-center duration-200 ease-in-out hover:scale-110 "
+          >
+            <img width={"40px"} src="/linkedin.png" alt="linkedin icon" />
+          </Link>
+          <Link
+            href={"https://github.com/thayanemenezes"}
+            className="origin-center duration-200 ease-in-out hover:scale-110"
+          >
+            <img width={"40px"} src="/github.png" alt="github icon" />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ Documents/personal/my-portfolio/app/components/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Content() {
+  return (
+    <div className="w-full">
+      <h1 className="lg:mt-20 text-gray-800">Hi, i'm Thayane.</h1>
+      <p className="text-gray-800 lg:py-10">
+        Full stack web and mobile developer, from northeastern Brazil,
+        passionate about technology and the possibility of changing lives
+        through it.
+      </p>
+    </div>
+  );
+}
